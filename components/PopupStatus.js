4cc456c0917e44a8deae86cecb3e13ed94a06e05
Ray <raymondly96@gmail.com>
2022-11-07 02:13:21 -0800

    TODO: Fix jank status icons

--- components/PopupStatus.js
+++ components/PopupStatus.js
@@ -60,15 +60,14 @@ export default function PopupStatus({ activeStatuses, setActiveStatuses, isOverf
 
         <Popover.Panel className="absolute top-[49px] z-50 left-0">
           <div className="bg-gray-900 border border-gray-600 p-2 rounded-md">
-                <div
-                  className='absolute w-0 h-0 bg-transparent left-5 -top-7 border-[16px] 
-                  border-gray-900 border-t-transparent border-r-transparent border-l-transparent
-                  border-b-gray-900'
-                >
-                  <div className='absolute bg-gray-600 w-[18px] h-[1px] rotate-45 top-[5px] -right-[15px]' />
-                  <div className='absolute bg-gray-600 w-[17px] h-[1px] -rotate-45 top-[5px] -left-[14.5px]' />
-                </div>
-
+            <div
+              className='absolute w-0 h-0 bg-transparent left-5 -top-7 border-[16px] 
+              border-gray-900 border-t-transparent border-r-transparent border-l-transparent
+              border-b-gray-900'
+            >
+              <div className='absolute bg-gray-600 w-[18px] h-[1px] rotate-45 top-[5px] -right-[15px]' />
+              <div className='absolute bg-gray-600 w-[17px] h-[1px] -rotate-45 top-[5px] -left-[14.5px]' />
+            </div>
 
             <form className='flex space'
               onSubmit={(e) => addStatus(e, close)}
