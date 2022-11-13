03b1c304bba60760be7c2f2e9500e24b9b8dd866
Ray <raymondly96@gmail.com>
2022-11-12 23:10:36 -0800

    Added sliding function to cards

--- components/DisplayCardCreature.js
+++ components/DisplayCardCreature.js
@@ -1,18 +1,21 @@
 import React, { useEffect, useRef, useState } from 'react'
-import { EllipsisHorizontalCircleIcon } from '@heroicons/react/24/solid'
-import Image from 'next/image'
+import { EllipsisHorizontalCircleIcon, ChevronLeftIcon, XMarkIcon } from '@heroicons/react/24/solid'
 import PopupStatus from './PopupStatus'
 import PopupAc from './PopupAc'
 import PopupHp from './PopupHp'
 import PopupOverflow from './PopupOverflow'
 import StatusIcon from './StatusIcon'
+import { activeCreaturesState } from '../atoms/activeCreaturesAtom'
+import { useRecoilState } from 'recoil'
 
 export default function DisplayCardCreature({ creatureData, windowSize }) {
+  const [activeCreatures, setActiveCreatures] = useRecoilState(activeCreaturesState);
   const [activeStatuses, setActiveStatuses] = useState([]);
   const [overflowStatuses, setOverflowStatuses] = useState([]);
   const [isHoverEllipses, setIsHoverEllipses] = useState(false);
   const [isOverflowed, setIsOverflowed] = useState(false);
   const [isAvailableSpace, setIsAvailableSpace] = useState(false);
+  const [isSlid, setIsSlid] = useState(false)
   const statusBarRef = useRef(null);
   const statusBarInnerRef = useRef(null);
   const cardRef = useRef(null);
@@ -22,7 +25,6 @@ export default function DisplayCardCreature({ creatureData, windowSize }) {
     const inner = statusBarInnerRef.current
     let isOverflowing = elt.clientWidth < elt.scrollWidth;
     let isSpace = elt.clientWidth - inner.clientWidth > 30;
-    // console.log(elt.clientWidth - inner.clientWidth)
 
     if (isOverflowing)
       setIsOverflowed(true)
@@ -61,85 +63,112 @@ export default function DisplayCardCreature({ creatureData, windowSize }) {
   }, [windowSize])
  
   return (
-    <div 
-      className='flex fix-borders py-3 border border-gray-700 bg-gray-800 text-stone-300 
-      rounded-md shadow-md relative space-x-2 px-3 w-full'
-      ref={cardRef}
-    >
-      {/* Name */}
-      <div className='cursor-pointer'>
-        {creatureData.name}
-      </div>
-      {/* Hp */}
-      <PopupHp 
-        creatureData={creatureData}
-      />
-      {/* AC */}
-      <PopupAc 
-        creatureData={creatureData}
-      />
-      {/* Statuses */}
+    <div className='relative rounded-md border border-gray-700 fix-borders z-auto'>
+      {/* Underlay */}
       <div 
-        className='flex flex-1 items-center space-x-2 px-1 overflow-visible'
-        ref={statusBarRef}
-      >        
-        <div className='inline-flex space-x-2 overflow-visible items-center'
-          ref={statusBarInnerRef}
+        className='absolute bg-gray-900 top-0 left-0 w-full h-full rounded-md flex 
+        justify-end items-center'
+      >
+        <div className='bg-red-500 rounded-md mr-1.5 p-1 cursor-pointer'
+          onClick={() => console.log('clicked')}
         >
-          {
-            activeStatuses.map((stat) => (
-              <StatusIcon 
-                key={stat.id}
-                stat={stat}
+          <XMarkIcon className='w-5 h-5 text-white'/>
+        </div>
+      </div>
+
+      {/* Main Card */}
+      <div 
+        className={`flex py-3 border-0 bg-gray-800 text-stone-300
+        rounded-md shadow-md relative top-0 left-0 px-3 w-full space-x-2 pr-1 transition-transform duration-200
+        ${isSlid && '-translate-x-10'}`}
+        ref={cardRef}
+      >
+        <div className={`flex w-full space-x-2 top-0 left-0 ${isSlid && 'pointer-events-none'}`}>
+          {/* Name */}
+          <div className='cursor-pointer'>
+            {creatureData.name}
+          </div>
+          {/* Hp */}
+          <PopupHp 
+            creatureData={creatureData}
+          />
+          {/* AC */}
+          <PopupAc 
+            creatureData={creatureData}
+          />
+          {/* Statuses */}
+          <div 
+            className='flex flex-1 items-center space-x-2 px-1 overflow-visible'
+            ref={statusBarRef}
+          >        
+            <div className='inline-flex space-x-2 overflow-visible items-center'
+              ref={statusBarInnerRef}
+            >
+              {
+                activeStatuses.map((stat) => (
+                  <StatusIcon 
+                    key={stat.id}
+                    stat={stat}
+                    activeStatuses={activeStatuses}
+                    setActiveStatuses={setActiveStatuses}
+                    overflowStatuses={overflowStatuses}
+                    setOverflowStatuses={setOverflowStatuses}
+                  />
+                ))
+              }
+
+              {
+                overflowStatuses.length > 0 &&
+                (
+                  <div
+                    className='z-40 block'
+                    onClick={(e) => setIsHoverEllipses(!isHoverEllipses)}
+                    onMouseEnter={(e) => setIsHoverEllipses(true)}  
+                    onMouseLeave={(e) => setIsHoverEllipses(false)}
+                  >
+                    <EllipsisHorizontalCircleIcon className='w-4 h-4' />
+                  </div>
+                )
+              }        
+              <PopupStatus
                 activeStatuses={activeStatuses}
                 setActiveStatuses={setActiveStatuses}
                 overflowStatuses={overflowStatuses}
                 setOverflowStatuses={setOverflowStatuses}
+                isOverflowed={isOverflowed}
+                isAvailableSpace={isAvailableSpace}
               />
-            ))
-          }
+              {
+                overflowStatuses.length == 0 && 
+                (
+                  <div className='w-4 h-4 bg-transparent shrink-0'></div>
+                )
+              }
+            </div>
+          </div>
 
-          {
-            overflowStatuses.length > 0 &&
-            (
-              <div
-                className='z-40 block'
-                onClick={(e) => setIsHoverEllipses(!isHoverEllipses)}
-                onMouseEnter={(e) => setIsHoverEllipses(true)}  
-                onMouseLeave={(e) => setIsHoverEllipses(false)}
-              >
-                <EllipsisHorizontalCircleIcon className='w-4 h-4' />
-              </div>
-            )
-          }        
-          <PopupStatus
-            activeStatuses={activeStatuses}
-            setActiveStatuses={setActiveStatuses}
+          {/* Overflow status popup */}
+          <PopupOverflow 
+            isHoverEllipses={isHoverEllipses}
             overflowStatuses={overflowStatuses}
-            setOverflowStatuses={setOverflowStatuses}
-            isOverflowed={isOverflowed}
-            isAvailableSpace={isAvailableSpace}
           />
-          {
-            overflowStatuses.length == 0 && 
-            (
-              <div className='w-4 h-4 bg-transparent shrink-0'></div>
-            )
-          }
+
+          {/* Initiative */}
+          <div className='font-bold select-none'>
+            {creatureData.initiative}
+            {/* ( {creatureData.dex_bonus}  */}
+          </div> 
+        </div>
+
+        {/* Expand Menu */}
+        <div className='flex items-center border-l border-l-gray-600 pl-1'>
+          <ChevronLeftIcon 
+            className={`w-5 h-5 cursor-pointer duration-200 transition-transform ${ isSlid && 'rotate-180'}`}
+            onClick={() => setIsSlid(!isSlid)}
+          />
         </div>
       </div>
-
-      {/* Overflow status popup */}
-      <PopupOverflow 
-        isHoverEllipses={isHoverEllipses}
-        overflowStatuses={overflowStatuses}
-      />
-
-      {/* Initiative */}
-      <div className='font-bold select-none'>
-        {creatureData.initiative}
-        {/* ( {creatureData.dex_bonus}  */}
-      </div> 
+       
     </div>
   )
 }
